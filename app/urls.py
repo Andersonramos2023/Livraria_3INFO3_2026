da3fb584ed77c75687a8edbb5db04370c3527e89
andersonrsanjo@gmail.com <andersonrsanjo@gmail.com>
2026-04-03 14:06:27 -0300

    FEAT: Criação da API para Categoria

--- app/urls.py
+++ app/urls.py
@@ -12,10 +12,11 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from core.views import UserRegistrationView, UserViewSet
+from core.views import CategoriaViewSet, UserViewSet, UserRegistrationView
 
 router = DefaultRouter()
 
+router.register(r'categorias', CategoriaViewSet, basename='categoria')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
