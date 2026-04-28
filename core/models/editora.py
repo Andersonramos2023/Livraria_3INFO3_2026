3847d0fde5f4815af6dc4002627006ece71fc177
Andersonramos2023 <Andersonrsanjo@gmail.com>
2026-04-28 16:43:41 -0300

    13. Dump e Load de dados

--- core/models/editora.py
+++ core/models/editora.py
@@ -2,7 +2,9 @@ from django.db import models
 
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
-    site = models.URLField(max_length=200, null='true', blank='true')
+    site = models.URLField(max_length=200, null=True,blank=True)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    cidade = models.CharField(max_length=100, blank=True, null=True)
 
 def __str__(self):
     return self.nome
