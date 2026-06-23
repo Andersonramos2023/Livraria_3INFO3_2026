38dc3b2618d4f0a95b0b89b2c0f5007e7ef047ef
andersonramos2023 <andersonrsanjo@gmail.com>
2026-06-23 09:23:32 -0300

    FIX - 24

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -4,21 +4,20 @@ from attr import field
 from rest_framework.serializers import ModelSerializer
 from rest_framework.serializers import CharField, ModelSerializer
 from core.models import Compra, ItensCompra
-from core.models import Compra
 
-class CompraSerializer(ModelSerializer):
-    usuario = CharField(source='usuario.email', read_only=True)
-
-class CompraSerializer(ModelSerializer):
-    status = CharField(source='get_status_display', read_only=True)
-
-class CompraSerializer(ModelSerializer):
-    class Meta:
-        model = Compra
-        fields = '__all__'
 
 class ItensCompraSerializer(ModelSerializer):
     class Meta:
      model = ItensCompra
-     fields = ('livro','quantidade')
-     Depth = 1
+    fields = ('livro', 'quantidade')     
+    Depth = 1
+
+class CompraSerializer(ModelSerializer):
+
+    class Meta:
+        model = Compra
+        itens = ItensCompraSerializer(many=True, read_only=True)
+        fields = '__all__'
+
+    usuario = CharField(source='usuario.email', read_only=True)
+    status = CharField(source='get_status_display',read_only=True)
