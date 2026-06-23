73d059d772bba221c5bb359b97182502db3892cb
andersonramos2023 <andersonrsanjo@gmail.com>
2026-06-23 08:54:05 -0300

    FIX(aula-23): alteração no compra.py no serializers.

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,6 +1,13 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import CharField, ModelSerializer
 
 from core.models import Compra
+
+class CompraSerializer(ModelSerializer):
+    usuario = CharField(source='usuario.email', read_only=True)
+    
+class CompraSerializer(ModelSerializer):
+    status = CharField(source='get_status_display', read_only=True)
 
 class CompraSerializer(ModelSerializer):
     class Meta:
