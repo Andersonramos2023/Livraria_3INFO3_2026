b8f448f112fc531add5bd476857a7f42148035c9
Andersonramos2023 <Andersonrsanjo@gmail.com>
2026-09-22 08:40:51 -0300

    FAET(aula-30): filtrando apenas as compras do usuário autenticado

--- core/views/compra.py
+++ core/views/compra.py
@@ -1,17 +1,17 @@
-from venv import create
 from rest_framework.viewsets import ModelViewSet
-from core.serializers import CompraCreateUpdateSerializer, CompraListSerializer,CompraSerializer
 from core.models import Compra
+from core.serializers.compra import (
+    CompraCreateUpdateSerializer,
+    CompraListSerializer,
+    CompraSerializer,
+)
 
 
 class CompraViewSet(ModelViewSet):
-    queryset = Compra.objects.order_by('-id')
-    serializer_class = CompraSerializer
-    http_method_names = ['get', 'post', 'put', 'delete']
-
-    def get_serializer_class(self):
-        if self.action == 'list':
-            return CompraListSerializer
-        if self.action in {'create', 'update'}:
-            return CompraCreateUpdateSerializer
-        return CompraSerializer
+    def get_queryset(self):
+        usuario = self.request.user
+        if usuario.is_superuser:
+            return Compra.objects.all()
+        if usuario.groups.filter(name='administradores'):
+            return Compra.objects.all()
+        return Compra.objects.filter(usuario=usuario)
